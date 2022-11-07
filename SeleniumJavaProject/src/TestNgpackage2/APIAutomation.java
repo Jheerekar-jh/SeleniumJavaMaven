58c1062f23d111c26c1d4c147686b829a62b96ba
hjaswanth7@gmail.com <hjaswanth7@gmail.com>
2022-11-08 08:35:21 +1000

    EndtoEnd

--- SeleniumJavaProject/src/TestNgpackage2/APIAutomation.java
+++ SeleniumJavaProject/src/TestNgpackage2/APIAutomation.java
@@ -1,16 +1,18 @@
 package TestNgpackage2;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class APIAutomation {
-	
-	@Test(groups={"Smoke"})
-	public void api()
+	@Parameters({"APIautomationURL"})
+	@Test(groups={"Smoke"},dependsOnMethods= {"api2"})
+	public void api(String uname)
 	{
-		System.out.println("this is api from API Automation");
+		System.out.println("this is API from API Automation");
+		System.out.println("This is API URL" + " " +uname);
 	}
 	
-	@Test
+	@Test(enabled=false)
 	public void api1()
 	{
 		System.out.println("this is ap1 from API automation");
@@ -20,4 +22,35 @@ public class APIAutomation {
 	{
 		System.out.println("this is ap2 from API automation");
 	}
+	@Test(groups={"Smoke"})
+	public void selenium()
+	{
+		System.out.println("this is selenium from selenium Automation");
+	}
+	@Test
+	public void selenium1()
+	{
+		System.out.println("this is selenium1 from selenium automation");
+	}
+	@Test
+	public void selenium2()
+	{
+		System.out.println("this is selenium2 from selenium automation");
+	}
+	@Test(groups={"Smoke"})
+	public void mobile()
+	{
+		System.out.println("this is mobile from mobile Automation");
+	}
+	@Test
+	public void mobile1()
+	{
+		System.out.println("this is mobile1 from mobile automation");
+	}
+	@Test
+	public void mobile2()
+	{
+		System.out.println("this is mobile2 from mobile automation");
+	}
+
 }
