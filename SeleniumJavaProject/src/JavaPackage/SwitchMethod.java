cf0c6f126856282842e61ce8634c84fb2766f70a
jheerekar-jh <hjaswanth7@gmail.com>
2022-12-13 22:25:16 +1000

    This is Java methods

---
+++ SeleniumJavaProject/src/JavaPackage/SwitchMethod.java
@@ -0,0 +1,27 @@
+package JavaPackage;
+
+public class SwitchMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 String browser="Chrome";
+
+		switch (browser) {
+		case "Chrome":
+			System.out.println("This is Chrome Browser");
+			break;
+		case "Firefox":
+			System.out.println("This is Firefox Browser");
+			break;
+		case "Edge":
+			System.out.println("This is Edge Browser");
+			break;
+		default:
+			System.out.println("Please select correct Browser");
+
+		}
+
+	}
+
+}
---
+++ SeleniumJavaProject/src/JavaPackage/methodsClass.java
@@ -0,0 +1,18 @@
+package JavaPackage;
+
+public class methodsClass {
+
+	public static void greetUser(String name)
+	{
+		System.out.println("Hi There " + " "+ name);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		greetUser("Jaswanth");
+		
+
+	}
+
+}
